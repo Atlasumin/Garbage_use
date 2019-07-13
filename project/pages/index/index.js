a3813eb867b6a1425fd19003e3412fee304d0b8d
FanCHEN <1670867189@qq.com>
2019-07-13 12:13:35 +0800

    完成了小程序的搜索功能

--- project/pages/index/index.js
+++ project/pages/index/index.js
@@ -37,6 +37,65 @@ Page({
       inputVal: e.detail.value
     });
   },
+  //isLike用于判断两个字符串中有没有相同的字，如果有，则返回true
+  isLike: function(str1,str2){
+    if(str1 === str2){
+      return true;
+    }
+    for(var i = 0; i<str1.length; i++){
+      if (str2.indexOf(str1[i]) != -1){
+        return true;
+      }
+    }
+    for (var i = 0; i < str2.length; i++) {
+      if (str1.indexOf(str2[i]) != -1) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  search: function(e){
+    //准备数据
+    let keywords = e.currentTarget.dataset.keywords;
+    let src = this.data.src;
+    let result = {};
+
+    //遍历src，调用isLike，将结果按照分类标准放入一个结果对象中。
+    for(var k in src){
+      //这一层遍历大分类，并将查询数据整合到result
+      var bigClass = {};
+      bigClass['detail'] = [];
+      for(var k2 in src[k]['detail']){
+        //这一层遍历小分类，并将查询数据整合到bigClass
+        var smallClass = {}; 
+        smallClass['example'] = [];
+        var oneDetail = src[k]['detail'][k2];
+        for(var k3 in oneDetail['example']){
+          //这一层遍历一个小分类中的所有关键词，并将查询结果整合到smallClass中
+          var str = oneDetail['example'][k3];
+          if(this.isLike(keywords,str) == true){
+            smallClass['example'].push(str);
+          }
+        }
+        if(smallClass['example'].length != 0){
+          smallClass['name'] = oneDetail['className'];
+          bigClass['detail'].push(smallClass);
+        }
+      }
+      if (bigClass['detail'].length != 0){
+        bigClass['name'] = src[k]['name'];
+        result[k] = bigClass;
+      }
+    }
+    
+    //跳转页面到搜索结果，发送数据\
+    let srcStr = JSON.stringify(src);
+    let jsonStr = JSON.stringify(result);
+    wx.navigateTo({
+      url: '../search_result/search_result?srcStr=' + srcStr +'&jsonStr=' + jsonStr + '&keywords=' + keywords,
+    })
+  },
 
   //导航栏
   onLoad: function () {
@@ -52,6 +111,7 @@ Page({
         //console.log(res.data.imgListData[0].tag)
         //将获取到的json数据，存在名字叫list_data的这个数组中
         that.setData({
+          src: res.data,
           page1_data: res.data.recyclable,
           page2_data: res.data.hazardous,
           page3_data: res.data.household,
