1a8753933455bdc9aa5b7c99f81b037e290c7bea
31601345_chenfan <a360show@163.com>
2019-07-14 13:07:44 +0800

    完成了首页图片

--- project/pages/index/index.js
+++ project/pages/index/index.js
@@ -7,7 +7,7 @@ Page({
     inputShowed: false,
     inputVal: "",
     isShowHis: false,
-    hisData:[],
+    hisData: [],
 
     //导航栏数据
     tabs: ["可回收垃圾", "有害垃圾", "干垃圾", "湿垃圾"],
@@ -26,7 +26,7 @@ Page({
       success(res) {
         console.log(res.data)
         that.setData({
-          hisData:res.data,
+          hisData: res.data,
         })
       }
     })
@@ -54,12 +54,12 @@ Page({
     });
   },
   //isLike用于判断两个字符串中有没有相同的字，如果有，则返回true
-  isLike: function(str1,str2){
-    if(str1 === str2){
+  isLike: function (str1, str2) {
+    if (str1 === str2) {
       return true;
     }
-    for(var i = 0; i<str1.length; i++){
-      if (str2.indexOf(str1[i]) != -1){
+    for (var i = 0; i < str1.length; i++) {
+      if (str2.indexOf(str1[i]) != -1) {
         return true;
       }
     }
@@ -71,40 +71,40 @@ Page({
     return false;
   },
 
-  search: function(e){
+  search: function (e) {
     //准备数据
     let keywords = e.currentTarget.dataset.keywords;
     let src = this.data.src;
     let result = {};
 
     //遍历src，调用isLike，将结果按照分类标准放入一个结果对象中。
-    for(var k in src){
+    for (var k in src) {
       //这一层遍历大分类，并将查询数据整合到result
       var bigClass = {};
       bigClass['detail'] = [];
-      for(var k2 in src[k]['detail']){
+      for (var k2 in src[k]['detail']) {
         //这一层遍历小分类，并将查询数据整合到bigClass
-        var smallClass = {}; 
+        var smallClass = {};
         smallClass['example'] = [];
         var oneDetail = src[k]['detail'][k2];
-        for(var k3 in oneDetail['example']){
+        for (var k3 in oneDetail['example']) {
           //这一层遍历一个小分类中的所有关键词，并将查询结果整合到smallClass中
           var str = oneDetail['example'][k3];
-          if(this.isLike(keywords,str) == true){
+          if (this.isLike(keywords, str) == true) {
             smallClass['example'].push(str);
           }
         }
-        if(smallClass['example'].length != 0){
+        if (smallClass['example'].length != 0) {
           smallClass['name'] = oneDetail['className'];
           bigClass['detail'].push(smallClass);
         }
       }
-      if (bigClass['detail'].length != 0){
+      if (bigClass['detail'].length != 0) {
         bigClass['name'] = src[k]['name'];
         result[k] = bigClass;
       }
     }
-    
+
     //跳转页面到搜索结果，发送数据\
     let srcStr = JSON.stringify(src);
     let jsonStr = JSON.stringify(result);
@@ -112,11 +112,11 @@ Page({
     this.clearInput();
     this.hideInput();
     let tempList = this.data.hisData;
-    if(tempList.indexOf(keywords) != -1){//当历史记录中有当前搜索的内容时，将该内容前移
-      tempList.splice(tempList.indexOf(keywords),1);
+    if (tempList.indexOf(keywords) != -1) {//当历史记录中有当前搜索的内容时，将该内容前移
+      tempList.splice(tempList.indexOf(keywords), 1);
       tempList.unshift(keywords);
-    }else{//当历史记录中没有时，判断数组元素是否到了6个，如果到了就删除最后一个记录，将新记录插入第一个位置
-      if (tempList.length >= 6){
+    } else {//当历史记录中没有时，判断数组元素是否到了6个，如果到了就删除最后一个记录，将新记录插入第一个位置
+      if (tempList.length >= 6) {
         tempList.pop();
       }
       tempList.unshift(keywords);
@@ -125,10 +125,10 @@ Page({
       key: "hisData",
       data: tempList,
     })
-    
+
 
     wx.navigateTo({
-      url: '../search_result/search_result?srcStr=' + srcStr +'&jsonStr=' + jsonStr + '&keywords=' + keywords,
+      url: '../search_result/search_result?srcStr=' + srcStr + '&jsonStr=' + jsonStr + '&keywords=' + keywords,
     })
   },
 
@@ -136,6 +136,8 @@ Page({
   onLoad: function () {
     //json数据下载和绑定
     var that = this
+
+    /*
     wx.request({
       url: 'https://www.233yo.cn/trash.json', //json数据地址
       headers: {
@@ -157,6 +159,32 @@ Page({
         //console.log(res.data.recyclable.detail[0])
       }
     })
+    */
+
+
+    wx.cloud.init({
+      env: 'zucc31601345-3t3em'
+    })
+    const db = wx.cloud.database({
+      env:'zucc31601345-3t3em'
+    })
+    db.collection('test').doc('wVBqJ34VPpRtLOWaV4k4QHq4eO2BaA2RaK3CV17URns1yqpG').get({
+      success: function (res) {
+        //将获取到的json数据，存在名字叫list_data的这个数组中
+        that.setData({
+          src: res.data,
+          page1_data: res.data.recyclable,
+          page2_data: res.data.hazardous,
+          page3_data: res.data.household,
+          page4_data: res.data.residual,
+          //res代表success函数的事件对，data是固定的，imgListData是上面json数据中imgListData
+        })
+      }
+    })
+
+
+
+
 
     wx.getSystemInfo({
       success: function (res) {
